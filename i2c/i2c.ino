68bdcb3e5d370feb82788ba556d913aeb9c463f8
MelcherS88 <Simon_Melcher@web.de>
2025-03-01 15:36:37 +0100

    changed i2c pin from 8,9 to 0,1 at pico

--- i2c/i2c.ino
+++ i2c/i2c.ino
@@ -4,15 +4,38 @@
 #define SCL_PIN D1         //Declare SDA Pin on NodeMCU
 #define ADDRESS 5
 
+//char msg[] = "";
+String msg = "";
+
+void add_char(char msg[], String addition){
+  for(int i = 0; i<sizeof(addition); i++){
+    int l = sizeof(msg);
+    msg[l+1] = addition[i];
+  }
+}
+
 void setup() {
  Wire.begin(SCL_PIN, SDA_PIN,ADDRESS); //start I2C communication on address 8
  Wire.onReceive(receiveEvent);   //register receive event
  Wire.onRequest(requestEvent);   //register request event
  Serial.begin(9600);             //start serial for debug
+
+ //add_char(msg, "Hallo Pico");
+ //add_char(msg, "$");
+ //add_char(msg, "1,2,3");
+
+ //String msg = String(msg);
+ //Serial.print(msg);
+ char sonderzeichen = 36;
+
+ msg.concat("Hallo Poco");
+ msg.concat(String(sonderzeichen));
+ msg.concat("1,2,3");
+ Serial.print(msg);
 }
 
 void loop() {
- //Serial.print(".");
+ //Serial.print("\n" + msg);
  //delay(1000);
 }
 
@@ -27,5 +50,5 @@ void receiveEvent(int howMany) {
 
 // function that executes whenever data is requested from master
 void requestEvent() {
- Wire.write("HELLO_PICO");  //send string on request
+ Wire.write(msg.c_str());  //send string on request
 }
--- main.py
+++ main.py
@@ -4,23 +4,33 @@ import machine
 from machine import I2C,Pin
 
 def main():
+    print("main")
     led = machine.Pin(25, machine.Pin.OUT)
+    led.value(1)
+    utime.sleep(0.5)
     led.value(0)
 
     I2C_ADDR = 0x05 #I2C Address of NodeMCU
     data = "HELLO_NODEMCU" #Data to send to I2C device
 
-    i2c = I2C(0, sda=machine.Pin(8), scl=machine.Pin(9), freq=10000)
+    #try:
+    i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=10000)
     print(i2c)
     print("Initalizing I2C as Master")
 
     while True:
         i2c.writeto(I2C_ADDR, data) #This line is responsible for sending data to nodemcu 
         a = i2c.readfrom(I2C_ADDR,10) #This line responsible for read data from Nodemcu
+        #a.decode("iso8859")
         led.value(1)
         print(a)
         utime.sleep(0.1)
         led.value(0)
         utime.sleep(0.1)
+
+    #except:
+    #    print("cannot connect")
+        
+        
 
 main()
